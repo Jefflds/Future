e282889387abe8ce090599345a418945880f0fe8
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2023-12-19 01:50:47 +0000

    Estruturação da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Footer from "./Layout/Footer/Footer";
+import Header from "./Layout/Header/Header";
+import Main from "./Layout/Main/Main";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
