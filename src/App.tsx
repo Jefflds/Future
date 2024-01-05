1a59d5d6402eced183c1d2b8937a45da7351e06d
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-04 21:18:35 -0300

    Componentizando Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import { GlobalStyle } from "./ui/styles/global";
+import { GlobalStyle } from "./ui/styles/Global";
 import "./style.css";
+import Sidebar from "./ui/components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <nav>
-        <ul>
-          <li>
-            <div className="home-icon">
-              <div className="roof">
-                <div className="roof-edge"></div>
-              </div>
-              <div className="front"></div>
-            </div>
-          </li>
-          <li>
-            <div className="about-icon">
-              <div className="head">
-                <div className="eyes"></div>
-                <div className="beard"></div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="work-icon">
-              <div className="paper"></div>
-              <div className="lines"></div>
-              <div className="lines"></div>
-              <div className="lines"></div>
-            </div>
-          </li>
-          <li>
-            <div className="mail-icon">
-              <div className="mail-base">
-                <div className="mail-top"></div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </nav>
+      <Sidebar />
     </>
   );
 }
